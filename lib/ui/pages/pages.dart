988b5a1ae15277e1b15560f24bc10fec16cff495
ijlalbm <muhammadijlalm@gmail.com>
2022-02-20 19:04:02 +0800

    custom navbar

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -4,6 +4,7 @@ import 'package:bwa_flutix/shared/shared.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:email_validator/email_validator.dart';
