804f91530b668872c9f39a4f5ce226c6a61b6524
ijlalbm <muhammadijlalm@gmail.com>
2022-03-02 16:21:31 +0800

    signup page part3

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:another_flushbar/flushbar.dart';
 import 'package:bwa_flutix/models/models.dart' as model;
 import 'package:bwa_flutix/services/services.dart';
@@ -6,12 +7,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:email_validator/email_validator.dart';
-
 import '../../bloc/blocs.dart';
 
 part 'main_page.dart';
@@ -22,3 +23,13 @@ part 'movie_page.dart';
 part 'sign_up_page.dart';
 part 'preference_page.dart';
 part 'account_confirmation_page.dart';
+
+Future<File?> getImage() async {
+  XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
+  if (image == null) {
+    return null;
+  } else {
+    File newImage = File(image.path);
+    return newImage;
+  }
+}
--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -24,6 +24,11 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
+    context.read<ThemeBloc>().add(
+          ChangeTheme(
+            ThemeData().copyWith(),
+          ),
+        );
     return WillPopScope(
         onWillPop: () async {
           context.read<PageBloc>().add(GoToSplashPage());
@@ -55,8 +60,149 @@ class _SignUpPageState extends State<SignUpPage> {
                               color: Colors.black,
                             ),
                           ),
+                        ),
+                        Center(
+                          child: Text(
+                            "Create New\nAccount",
+                            style: blackTextFont.copyWith(
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
                         )
                       ]),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 104,
+                      child: Stack(children: <Widget>[
+                        Container(
+                          height: 90,
+                          width: 90,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: getProfilePicture(
+                                  widget.registrationData.profileImage ?? ''),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: GestureDetector(
+                            onTap: () async {
+                              // todo: ambil file pp
+                              if (widget.registrationData.profileImage ==
+                                  null) {
+                                widget.registrationData.profileImage =
+                                    await getImage();
+                              } else {
+                                widget.registrationData.profileImage = null;
+                              }
+                              setState(() {});
+                            },
+                            child: Container(
+                              height: 28,
+                              width: 28,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: AssetImage(widget.registrationData
+                                                  .profileImage ==
+                                              null
+                                          ? "assets/btn_add_photo.png"
+                                          : "assets/btn_del_photo.png"))),
+                            ),
+                          ),
+                        )
+                      ]),
+                    ),
+                    SizedBox(
+                      height: 36,
+                    ),
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Full Name",
+                        hintText: "Full Name",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Email",
+                        hintText: "Email",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Password",
+                        hintText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: retypePassWordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: "Confirm Password",
+                        hintText: "Confirm Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    FloatingActionButton(
+                      child: Icon(Icons.arrow_forward),
+                      backgroundColor: mainColor,
+                      elevation: 0,
+                      onPressed: () {
+                        if (!(nameController.text.trim() != "" &&
+                            emailController.text.trim() != "" &&
+                            passwordController.text.trim() != "" &&
+                            retypePassWordController.text.trim() != "")) {
+                          Flushbar(
+                            duration: Duration(milliseconds: 1500),
+                            flushbarPosition: FlushbarPosition.TOP,
+                            backgroundColor: Color(0xFFFFF5C83),
+                            message: 'Please fill all the fields',
+                          )..show(context);
+                        } else if (!EmailValidator.validate(
+                            emailController.text)) {
+                          Flushbar(
+                            duration: Duration(milliseconds: 1500),
+                            flushbarPosition: FlushbarPosition.TOP,
+                            backgroundColor: Color(0xFFFFF5C83),
+                            message: 'Wrong formatted email address',
+                          )..show(context);
+                        } else {
+                          widget.registrationData.name = nameController.text;
+                          widget.registrationData.email = emailController.text;
+                          widget.registrationData.password =
+                              passwordController.text;
+                        }
+                      },
                     )
                   ],
                 )
@@ -64,5 +210,13 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
           ),
         ));
+  }
+
+  ImageProvider getProfilePicture(var url) {
+    if (url == '') {
+      return AssetImage('assets/user_pic.png');
+    } else {
+      return FileImage(url);
+    }
   }
 }
